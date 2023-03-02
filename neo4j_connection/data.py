1390b8c5995335cf6fd685c37fc608a938cb0e9d
nonhana <1209220829@qq.com>
2023-03-02 20:44:51 +0800

    成功解决从Neo4j拿取相关数据并处理

--- neo4j_connection/data.py
+++ neo4j_connection/data.py
@@ -1,61 +1,107 @@
 from py2neo import Graph
 
+# 连接neo4j数据库
+
 
 class MedicalGraph:
     def __init__(self):
         self.g = Graph('http://localhost:7474/', user='neo4j',
                        password='20021209xiang', name='neo4j')
-        
 
+    # 查询
 
-    #查询
-    def query(tx, name):
-        content = []
-        link_list = []
-        id_list = []
-        # 模糊查询
-        statementrecord = tx.run("MATCH (n) WHERE n.name = " + name + " RETURN n")
-        # 查询结果存入字典
-        for record in statementrecord.records():
-            dic = dict(record['n'])
-            id_list.append(record['n'].id)
-            dic['id'] = record['n'].id
-            content.append(dic)
-        # 根据id查询关系节点
-        linkrecord = tx.run("MATCH (n)<-[r]->(x) WHERE id(n) in {list} RETURN id(n), labels(x), x.name, r", list=id_list)
-        dic = {}
-        count = 1
-        # 存入字典，将id相同的查询结果进行整合
-        for record in linkrecord.records():
-            idn = record['id(n)']
-            xlabels = record['labels(x)'][0].lower()
-            xname = record['x.name']
-            if dic.get('id') == idn:
-                if dic.get(xlabels):
-                    if xname not in dic[xlabels]:
-                        dic[xlabels].append(xname)
-                        count += 1
-                else:
-                    count += 1
-                    dic[xlabels] = [xname]
-            else:
-                if dic != {}:
-                    dic['cnt'] = count
-                    count = 1
-                    link_list.append(copy.copy(dic))
-                dic = {}
-                dic['id'] = idn
-                dic[xlabels] = [xname]
-        dic['cnt'] = count
-        link_list.append(dic)
-        for link in link_list:
-            for con in content:
-                try:
-                    if con['id'] == link['id']:
-                        con.update(link)
-                except:
-                    pass
-        return content
+    def query(self):
+        # 根节点
+        n_node = []
+        # 第一条边的相关属性
+        r1_startnode = []
+        r1_name = []
+        r1_endnode = []
+        # 第二节点
+        m_node = []
+        # 第二条边的相关属性
+        r2_startnode = []
+        r2_name = []
+        r2_endnode = []
+        # 第三节点
+        m1_node = []
+        # 第三条边的相关属性
+        r3_startnode = []
+        r3_name = []
+        r3_endnode = []
+
+        # 定义cql语句
+        cql = 'match (n:industry {name:"基础化工"})-[r1]-(m)-[r2]-(m1)-[r3]-(m2) return n,r1,m,r2,m1,r3,m2'
+
+        # 查询
+        n = self.g.run(cql).data('n')
+        r1 = self.g.run(cql).data('r1')
+        m = self.g.run(cql).data('m')
+        r2 = self.g.run(cql).data('r2')
+        m1 = self.g.run(cql).data('m1')
+        r3 = self.g.run(cql).data('r3')
+
+        # 根节点
+        for i in range(len(n)):
+            record = list(n[i].values())
+            result = list(record[0].values())[1]
+            n_node.append(result)
+        # print(n_node)
+
+        # 第一条边
+        for i in range(len(r1)):
+            record = list(r1[i].values())
+            result = str(record[0])
+            # 将字符串切片，取出头结点、边名称、尾节点
+            r1_startnode.append(result[result.index("(")+1:result.index(")")])
+            r1_name.append(result[result.index("[")+2:result.index("]")-3])
+            r1_endnode.append(
+                result[result.index("(", 5)+1:result.index(")", 10)])
+        # print(r1_startnode)
+        # print(r1_name)
+        # print(r1_endnode)
+
+        # 第二节点
+        for i in range(len(m)):
+            record = list(m[i].values())
+            result = list(record[0].values())[1]
+            m_node.append(result)
+        # print(m_node)
+
+        # 第二条边
+        for i in range(len(r2)):
+            record = list(r2[i].values())
+            result = str(record[0])
+            # 将字符串切片，取出头结点、边名称、尾节点
+            r2_startnode.append(result[result.index("(")+1:result.index(")")])
+            r2_name.append(result[result.index("[")+2:result.index("]")-3])
+            r2_endnode.append(
+                result[result.index("(", 5)+1:result.index(")", 10)])
+        # print(r2_startnode)
+        # print(r2_name)
+        # print(r2_endnode)
+
+        # 第三节点
+        for i in range(len(m1)):
+            record = list(m1[i].values())
+            result = list(record[0].values())[1]
+            m1_node.append(result)
+        # print(m1_node)
+
+        # 第三条边
+        for i in range(len(r3)):
+            record = list(r3[i].values())
+            result = str(record[0])
+            # 将字符串切片，取出头结点、边名称、尾节点
+            r3_startnode.append(result[result.index("(")+1:result.index(")")])
+            r3_name.append(result[result.index("[")+2:result.index("]")-3])
+            r3_endnode.append(
+                result[result.index("(", 5)+1:result.index(")", 10)])
+        # print(r3_startnode)
+        # print(r3_name)
+        # print(r3_endnode)
+
 
 if __name__ == '__main__':
     handler = MedicalGraph()
+    handler.query()
